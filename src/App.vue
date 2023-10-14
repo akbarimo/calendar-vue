2a7eb58c60ce43f49a8a0cecb6ccabc0d6dfb283
akbarimo <mo.akbari15@gmail.com>
2023-10-13 21:21:31 -0700

    Implement barebones calendar with simple styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
 <script setup>
-import { generateCalendarDays, months } from './utils/calendar';
-console.log(generateCalendarDays());
-console.log(months);
+import { generateCalendarDates, months, days } from './utils/calendar';
+const dates = generateCalendarDates();
 </script>
 <template>
-  <main>Hello World</main>
+  <main>
+    <div class="wrapper">
+      <div class="days-container">
+        <h1 class="dates" v-for="day in days" :key="day">{{ day }}</h1>
+      </div>
+      <div class="days-container">
+        <div class="dates" v-for="date in dates" :key="date.date">
+          <h1 :class="{ notCurrentMonth: !date.currentMonth }">{{ date.date.date() }}</h1>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  width: 24rem;
+  height: 24rem;
+}
+.days-container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(7, minmax(0, 1fr));
+}
+.dates {
+  height: 3.5rem;
+  display: grid;
+  place-content: center;
+}
+
+.notCurrentMonth {
+  color: grey;
+}
+</style>
--- src/utils/calendar.js
+++ src/utils/calendar.js
@@ -2,11 +2,11 @@ import dayjs from 'dayjs';
 import localeData from 'dayjs/plugin/localeData';
 dayjs.extend(localeData);
 
-export const generateCalendarDays = (month = dayjs().month(), year = dayjs().year()) => {
+export const generateCalendarDates = (month = dayjs().month(), year = dayjs().year()) => {
   const firstDateOfMonth = dayjs().year(year).month(month).startOf('month');
   const firstDayOfWeek = firstDateOfMonth.day();
 
-  const calendarDays = [...Array(42)].map((_, i) => {
+  const calendarDates = [...Array(42)].map((_, i) => {
     const currentDate = firstDateOfMonth.add(i - firstDayOfWeek, 'day');
     const isCurrentMonth = currentDate.isSame(firstDateOfMonth, 'month');
     return {
@@ -16,7 +16,9 @@ export const generateCalendarDays = (month = dayjs().month(), year = dayjs().yea
     };
   });
 
-  return calendarDays;
+  return calendarDates;
 };
 
 export const months = dayjs.months();
+
+export const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
